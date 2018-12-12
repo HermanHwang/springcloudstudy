56e575b37aed351071342dbb26958829f33abf94
huangzh <hwangzhcn@gmail.com>
2018-12-12 21:44:06 +0800

    添加Feign

---
+++ service-feign/src/main/java/com/forezp/servicefeign/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.forezp.servicefeign.web;
+
+import com.forezp.servicefeign.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hello")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHelloFromClientOne( name );
+    }
+}
